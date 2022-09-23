068e34fb5a1c0a6e40fffd80ef160ef72f7208d5
DESKTOP-O9E1V9C\Sasha <Sasha7b9@tut.by>
2022-09-23 22:27:23 +0300

    *

---
+++ sources/EarthPacker/Resources/Interface.cpp
@@ -0,0 +1,5 @@
+// 2022/09/23 22:21:25 (c) Aleksandr Shevchenko e-mail : Sasha7b9@tut.by
+#include "defines.h"
+#include "Resources/Interface.h"
+
+
---
+++ sources/EarthPacker/Resources/Interface.h
@@ -0,0 +1,8 @@
+// 2022/09/23 22:21:29 (c) Aleksandr Shevchenko e-mail : Sasha7b9@tut.by
+#pragma once
+
+
+namespace Resources
+{
+
+}
