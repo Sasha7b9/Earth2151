354dbac2cc391c26eb309c4cf226bc66c543929f
Sasha7b9Work <Phuntik-it@tut.by>
2022-09-23 13:09:40 +0300

    *

--- sources/scripts/ThirdParty/make.bat
+++ sources/scripts/ThirdParty/make.bat
@@ -1,2 +1,2 @@
 rmdir ..\..\ThirdParty\wxWidgets\generated /s /q
-cmake.exe ..\..\ThirdParty\wxWidgets\CMakeLists.txt -B..\..\generated\wxWidgets -DCMAKE_SYSTEM_VERSION=10.0.18362.0 -G "Visual Studio 17 2022" -A Win32
+cmake.exe ..\..\ThirdParty\wxWidgets\CMakeLists.txt -B..\..\generated\wxWidgets -DCMAKE_SYSTEM_VERSION=10.0.18362.0 -G "Visual Studio 17 2022" -A Win32 -DwxBUILD_SAMPLES=ALL
