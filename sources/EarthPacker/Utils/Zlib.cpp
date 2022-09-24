0b5f991dafd62ded5ce893a9acd7c57363126536
DESKTOP-O9E1V9C\Sasha <Sasha7b9@tut.by>
2022-09-24 11:33:50 +0300

    *

---
+++ sources/EarthPacker/Utils/Zlib.cpp
@@ -0,0 +1,5 @@
+// 2022/09/24 11:33:36 (c) Aleksandr Shevchenko e-mail : Sasha7b9@tut.by
+#include "defines.h"
+#include "Utils/Zlib.h"
+
+
---
+++ sources/EarthPacker/Utils/Zlib.h
@@ -0,0 +1,2 @@
+// 2022/09/24 11:33:24 (c) Aleksandr Shevchenko e-mail : Sasha7b9@tut.by
+#pragma once
