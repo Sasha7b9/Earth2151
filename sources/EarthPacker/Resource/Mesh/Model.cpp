fde34e18c50bbe717cfc09d6f4a561cdf1031b6f
DESKTOP-O9E1V9C\Sasha <Sasha7b9@tut.by>
2022-09-24 15:23:18 +0300

    *

--- sources/EarthPacker/Resource/Mesh/Model.cpp
+++ sources/EarthPacker/Resource/Mesh/Model.cpp
@@ -1,6 +1,6 @@
 // 2022/09/24 14:56:22 (c) Aleksandr Shevchenko e-mail : Sasha7b9@tut.by
 #include "defines.h"
-#include "Resource/Mesh/Mesh.h"
+#include "Resource/Mesh/Model.h"
 
 
 Resource::RMesh::RMesh(const wxString &path)
