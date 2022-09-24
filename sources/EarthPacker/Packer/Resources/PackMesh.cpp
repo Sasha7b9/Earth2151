4be126ebe10b1eea32177709bbe97792715c0a21
DESKTOP-O9E1V9C\Sasha <Sasha7b9@tut.by>
2022-09-24 13:19:34 +0300

    refact

--- sources/EarthPacker/Packer/Resources/PackMesh.cpp
+++ sources/EarthPacker/Packer/Resources/PackMesh.cpp
@@ -1,6 +1,6 @@
 // 2022/09/23 21:52:25 (c) Aleksandr Shevchenko e-mail : Sasha7b9@tut.by
 #include "defines.h"
-#include "Packer/Resources/Mesh.h"
+#include "Packer/Resources/PackMesh.h"
 
 
 using namespace Packer;
