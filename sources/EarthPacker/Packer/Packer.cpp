60dbe0a03f4ae8891d8f925a9baacb1e65741c83
Sasha7b9Work <Phuntik-it@tut.by>
2022-09-27 17:00:00 +0300

    *

---
+++ sources/EarthPacker/Packer/Packer.cpp
@@ -0,0 +1,4 @@
+// 2022/09/27 16:59:30 (c) Aleksandr Shevchenko e-mail : Sasha7b9@tut.by
+#include "defines.h"
+#include "Packer/Packer.h"
+
---
+++ sources/EarthPacker/Packer/Packer.h
@@ -0,0 +1,2 @@
+// 2022/09/27 16:59:23 (c) Aleksandr Shevchenko e-mail : Sasha7b9@tut.by
+#pragma once
