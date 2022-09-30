d500587181122010e5d79836bb076aaf00784dd9
DESKTOP-O9E1V9C\Sasha <Sasha7b9@tut.by>
2022-09-30 21:58:33 +0300

    *

---
+++ sources/EarthPacker/Controls/NotebookLeft/PageDirectory.cpp
@@ -0,0 +1,3 @@
+// 2022/09/30 21:46:14 (c) Aleksandr Shevchenko e-mail : Sasha7b9@tut.by
+#include "defines.h"
+#include "Controls/NotebookLeft/PageDirectory.h"
---
+++ sources/EarthPacker/Controls/NotebookLeft/PageDirectory.h
@@ -0,0 +1,2 @@
+// 2022/09/30 21:46:31 (c) Aleksandr Shevchenko e-mail : Sasha7b9@tut.by
+#pragma once
