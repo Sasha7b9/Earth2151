f986126497b3297a7df76ef1e2e3ca0944ef75ee
DESKTOP-O9E1V9C\Sasha <Sasha7b9@tut.by>
2022-10-02 20:32:07 +0300

    Refact

--- sources/EarthPacker/Packer/Packer.cpp
+++ sources/EarthPacker/Packer/Packer.cpp
@@ -12,6 +12,8 @@ namespace Packer
     static bool IsValidWDFile(wxFileInputStream &);
 
     static void CreateModel(const wxFileName &);
+
+    static void UnpackFile(const wxFileName &);
 }
 
 
@@ -28,85 +30,9 @@ void Packer::ProcessFile(const wxString &path)
 
     if (fileName.GetExt() == "wd")
     {
-        FileInputStream file(fileName.GetFullPath());
+        UnpackFile(fileName);
 
-        if (!IsValidWDFile(file))
-        {
-            return;
-        }
-
-        uint dirLn = file.ReadUINT(file.GetSize() - 4);
-
-        wxMemoryBuffer dirData = file.ReadBytes(file.GetSize() - dirLn, dirLn);
-
-        wxMemoryInputStream dirDataStream(dirData.GetData(), dirData.GetBufSize());
-
-        wxZlibInputStream zstream(dirDataStream);
-
-        wxMemoryOutputStream dir_stream;
-
-        zstream.Read(dir_stream);
-
-        size_t size_dir_stream = dir_stream.GetSize();
-
-        wxMemoryBuffer _dir(size_dir_stream);
-
-        dir_stream.CopyTo(_dir.GetData(), size_dir_stream);
-
-        auto dirdesc = new Packer::Directory(_dir);
-
-        for each (auto desc in dirdesc->resources)
-        {
-            if (desc.file_name.empty())
-            {
-                continue;
-            }
-
-            wxMemoryBuffer data = file.ReadBytes(desc.info.offset, desc.info.length);
-
-            if (data.GetBufSize())
-            {
-                wxFileName path_resource(fileName.GetPath() + wxFileName::GetPathSeparator() + desc.file_name);
-
-                wxString directory = path_resource.GetPath();
-
-                if (!wxDir::Exists(directory))
-                {
-                    wxDir::Make(directory);
-                }
-
-                wxFile file_resource;
-
-                file_resource.Create(path_resource.GetFullPath(), true);
-
-                data = (desc.info.decompressedLength == desc.info.length) ? data : Zlib::Decompress(data);
-
-                file_resource.Write(data.GetData(), data.GetBufSize());
-
-                if (desc.unknown_data.size())
-                {
-                    wxFile file_unknown;
-
-                    file_unknown.Create(fileName.GetPath() + wxFileName::GetPathSeparator() + desc.file_name + ".unknownData", true);
-
-                    for (uint i = 0; i < desc.unknown_data.size(); i++)
-                    {
-                        file_unknown.Write(&desc.unknown_data[i], 1);
-                    }
-                }
-
-                if (typeid(desc) == typeid(Packer::TranslatableResource))
-                {
-                    wxFile file_trans;
-
-                    file_trans.Create(fileName.GetPath() + wxFileName::GetPathSeparator() + desc.file_name + ".translationId", true);
-
-                    file_trans.Write(((Packer::TranslatableResource *)&desc)->translationID);
-                }
-            }
-        }
-
-        delete dirdesc;
+        return;
     }
 }
 
@@ -114,6 +40,90 @@ void Packer::ProcessFile(const wxString &path)
 void Packer::CreateModel(const wxFileName &file_name)
 {
     new Models::Model(file_name);
+}
+
+
+void Packer::UnpackFile(const wxFileName &file_name)
+{
+    FileInputStream file(file_name.GetFullPath());
+
+    if (!IsValidWDFile(file))
+    {
+        return;
+    }
+
+    uint dirLn = file.ReadUINT(file.GetSize() - 4);
+
+    wxMemoryBuffer dirData = file.ReadBytes(file.GetSize() - dirLn, dirLn);
+
+    wxMemoryInputStream dirDataStream(dirData.GetData(), dirData.GetBufSize());
+
+    wxZlibInputStream zstream(dirDataStream);
+
+    wxMemoryOutputStream dir_stream;
+
+    zstream.Read(dir_stream);
+
+    size_t size_dir_stream = dir_stream.GetSize();
+
+    wxMemoryBuffer _dir(size_dir_stream);
+
+    dir_stream.CopyTo(_dir.GetData(), size_dir_stream);
+
+    auto dirdesc = new Packer::Directory(_dir);
+
+    for each (auto desc in dirdesc->resources)
+    {
+        if (desc.file_name.empty())
+        {
+            continue;
+        }
+
+        wxMemoryBuffer data = file.ReadBytes(desc.info.offset, desc.info.length);
+
+        if (data.GetBufSize())
+        {
+            wxFileName path_resource(file_name.GetPath() + wxFileName::GetPathSeparator() + desc.file_name);
+
+            wxString directory = path_resource.GetPath();
+
+            if (!wxDir::Exists(directory))
+            {
+                wxDir::Make(directory);
+            }
+
+            wxFile file_resource;
+
+            file_resource.Create(path_resource.GetFullPath(), true);
+
+            data = (desc.info.decompressedLength == desc.info.length) ? data : Zlib::Decompress(data);
+
+            file_resource.Write(data.GetData(), data.GetBufSize());
+
+            if (desc.unknown_data.size())
+            {
+                wxFile file_unknown;
+
+                file_unknown.Create(file_name.GetPath() + wxFileName::GetPathSeparator() + desc.file_name + ".unknownData", true);
+
+                for (uint i = 0; i < desc.unknown_data.size(); i++)
+                {
+                    file_unknown.Write(&desc.unknown_data[i], 1);
+                }
+            }
+
+            if (typeid(desc) == typeid(Packer::TranslatableResource))
+            {
+                wxFile file_trans;
+
+                file_trans.Create(file_name.GetPath() + wxFileName::GetPathSeparator() + desc.file_name + ".translationId", true);
+
+                file_trans.Write(((Packer::TranslatableResource *)&desc)->translationID);
+            }
+        }
+    }
+
+    delete dirdesc;
 }
 
 
