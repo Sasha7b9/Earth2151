303de80cf80fd5a8bb2c37c5af2281df44ad2222
DESKTOP-O9E1V9C\Sasha <Sasha7b9@tut.by>
2022-10-02 20:39:43 +0300

    *

---
+++ sources/EarthPacker/Packer/DescriptionFile.cpp
@@ -0,0 +1,3 @@
+// 2022/10/02 20:38:25 (c) Aleksandr Shevchenko e-mail : Sasha7b9@tut.by
+#include "defines.h"
+#include "Packer/DescriptionFile.h"
---
+++ sources/EarthPacker/Packer/DescriptionFile.h
@@ -0,0 +1,9 @@
+// 2022/10/02 20:38:20 (c) Aleksandr Shevchenko e-mail : Sasha7b9@tut.by
+#pragma once
+
+
+class DescriptionFile : public std::vector<std::string>
+{
+public:
+private:
+};
