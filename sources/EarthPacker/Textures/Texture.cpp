0eecea60e224855d1eb289d9a03f324f6419523e
DESKTOP-O9E1V9C\Sasha <Sasha7b9@tut.by>
2022-11-26 15:42:05 +0300

    Подготовка к распаковке текстур

---
+++ sources/EarthPacker/Textures/Texture.cpp
@@ -0,0 +1,3 @@
+// 2022/11/26 15:41:06 (c) Aleksandr Shevchenko e-mail : Sasha7b9@tut.by
+#include "defines.h"
+#include "Textures/Texture.h"
---
+++ sources/EarthPacker/Textures/Texture.h
@@ -0,0 +1,2 @@
+// 2022/11/26 15:41:02 (c) Aleksandr Shevchenko e-mail : Sasha7b9@tut.by
+#pragma once
