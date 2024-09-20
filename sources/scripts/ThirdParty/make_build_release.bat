9259d7ee12e21d7a7cd2249b0f1ffb89fe83266f
Sasha7b9Work <Sasha7b9@tut.by>
2024-09-20 19:40:20 +0300

    Исправлен скрипт сборки wxWidgets

--- sources/scripts/ThirdParty/make_build_release.bat
+++ sources/scripts/ThirdParty/make_build_release.bat
@@ -1,5 +1,5 @@
 cd ../../ThirdParty/wxWidgets
 rmdir generated /s /q
-cd ../scripts
+cd ../../scripts/ThirdParty/
 cmake.exe ..\..\ThirdParty\wxWidgets\CMakeLists.txt -B..\..\generated\wxWidgets -G "Visual Studio 17 2022" -A Win32 -DwxBUILD_SHARED=ON -DwxBUILD_USE_STATIC_RUNTIME=ON
 call build.bat
