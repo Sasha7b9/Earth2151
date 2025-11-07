3edfb2cb557f2fab843af7702145bfdd720f1541
Sasha7b9Work <Sasha7b9@tut.by>
2025-11-07 15:11:40 +0300

    Переход wxWidgets на 3.3.1

--- sources/scripts/ThirdParty/make_build_release.bat
+++ sources/scripts/ThirdParty/make_build_release.bat
@@ -1,5 +1,5 @@
 cd ../../ThirdParty/wxWidgets
 rmdir generated /s /q
 cd ../../scripts/ThirdParty/
-cmake.exe ..\..\ThirdParty\wxWidgets\CMakeLists.txt -B..\..\generated\wxWidgets -G "Visual Studio 17 2022" -A Win32 -DwxBUILD_SHARED=ON -DwxBUILD_USE_STATIC_RUNTIME=ON
+cmake.exe ..\..\ThirdParty\wxWidgets\CMakeLists.txt -B..\..\generated\wxWidgets -G "Visual Studio 17 2022" -A Win32 -DwxBUILD_SHARED=ON -DwxBUILD_USE_STATIC_RUNTIME=ON -DwxBUILD_SAMPLES=OFF
 call build.bat
